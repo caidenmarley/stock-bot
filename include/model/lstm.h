11c7db99fa845db06ec9b5b956018ce9012e0731
Caiden <caidenmarley@icloud.com>
2025-07-10 17:49:55 +0100

    lstm preprocess before forward pass

---
+++ include/model/lstm.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <Eigen/Dense>
+#include <cmath>
+#include <random>
+
+class LSTMCell{
+public:
+    LSTMCell(int numFeatures, int hiddenSize);
+    void xavierWeightsInit();
+    inline void initWeights(Eigen::MatrixXd &W, std::mt19937 &rng, std::normal_distribution<double> &dist){
+        for (int i = 0; i < W.rows(); ++i) {
+            for (int j = 0; j < W.cols(); ++j) {
+                W(i, j) = dist(rng);
+            }
+        }       
+    }
+private:
+    int numFeatures;
+    int hiddenSize;
+
+    // Forget weight(Wf), hidden forget weight(Uf), forget bias(bf)
+    // forget gate output vector(Ft) = sigmoid(Wf*inputVec + Uf*PrevHiddenStateVec + bf)
+    Eigen::MatrixXd Wf, Uf;
+    Eigen::VectorXd bf;
+
+    // input weight(Wi), hidden input weight(Ui), input bias(bi)
+    // input gate output vector(It) = sigmoid(Wi*inputVec + Ui*PrevHiddenStateVec + bi)
+    Eigen::MatrixXd Wi, Ui;
+    Eigen::VectorXd bi;
+
+    // output weight(Wo), hidden output weight(Uo), output bias(bo)
+    // output gate output vector(Ot) = sigmoid(Wo*inputVec + Uo*PrevHiddenStateVec + bo)
+    Eigen::MatrixXd Wo, Uo;
+    Eigen::VectorXd bo;
+
+    // cell input weight(Wc), hidden cell input weight(Uc), cell input bias(bc)
+    // cell input vector(Ct) = tanh(Wc*inputVec + Uc*PrevHiddenStateVec + bc)
+    Eigen::MatrixXd Wc, Uc;
+    Eigen::VectorXd bc;
+
+    // cell state vector = (Ft Hadamard product previous cell state vector) + (It Hadarmard product Ct)
+    // hidden state vector = Ot Hadamard product tanh(cell state vector)
+    Eigen::VectorXd cellState, hiddenState;
+};
---
+++ src/model/lstm.cpp
@@ -0,0 +1,32 @@
+#include "model/lstm.h"
+#include <random>
+
+LSTMCell::LSTMCell(int numFeatures, int hiddenSize) 
+: numFeatures(numFeatures), hiddenSize(hiddenSize), 
+  cellState(Eigen::VectorXd::Zero(hiddenSize)), hiddenState(Eigen::VectorXd::Zero(hiddenSize)),
+  Wf(hiddenSize, numFeatures), Uf(hiddenSize, hiddenSize), bf(Eigen::VectorXd::Constant(hiddenSize, 1.0)), // set to 1 so lstm doesnt forget at the start
+  Wi(hiddenSize, numFeatures), Ui(hiddenSize, hiddenSize), bi(Eigen::VectorXd::Zero(hiddenSize)), 
+  Wo(hiddenSize, numFeatures), Uo(hiddenSize, hiddenSize), bo(Eigen::VectorXd::Zero(hiddenSize)),
+  Wc(hiddenSize, numFeatures), Uc(hiddenSize, hiddenSize), bc(Eigen::VectorXd::Zero(hiddenSize)){
+    xavierWeightsInit();
+}
+
+// For each weight draw a rngom value from a normal distribution with mean 0 and
+// standard deviation = sqrt(2/(numInputs + numOutputs))
+void LSTMCell::xavierWeightsInit(){
+    // static prevents reinitialsing mt19937 again (expensive time cost)
+    static std::mt19937 rng(std::random_device{}()); 
+
+    double stdDevInput = sqrt(2.0 / (static_cast<double>(numFeatures) + static_cast<double>(hiddenSize)));
+    double stdDevHidden = sqrt(2.0 / (static_cast<double>(hiddenSize) + static_cast<double>(hiddenSize)));
+    std::normal_distribution<double> normDistInput(0.0, stdDevInput);
+    std::normal_distribution<double> normDistHidden(0.0, stdDevHidden);
+    initWeights(Wf, rng, normDistInput);
+    initWeights(Uf, rng, normDistHidden);
+    initWeights(Wi, rng, normDistInput);
+    initWeights(Ui, rng, normDistHidden);
+    initWeights(Wo, rng, normDistInput);
+    initWeights(Uo, rng, normDistHidden);
+    initWeights(Wc, rng, normDistInput);
+    initWeights(Uc, rng, normDistHidden);
+}
