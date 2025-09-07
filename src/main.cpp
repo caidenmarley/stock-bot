cee4768bb839beb4afc76e1a41f59318754280f9
James <jamesleewallwork@gmail.com>
2025-09-07 11:45:36 +0100

    use cxxopts to parse args

--- src/main.cpp
+++ src/main.cpp
@@ -1,79 +1,105 @@
 #include "inputs/parser.h"
-#include "inputs/shape_inputs.h"
-#include "model/lstm.h"
-#include "model/ada_belief.h"
-#include "model/huber_loss_function.h"
-#include "model/dense.h"
 #include "model/trainer.h"
 #include "search/hyperparam_search.h"
+#include <cxxopts.hpp>
+#include <fmt/base.h>
 #include <iostream>
 #include <vector>
 
-int main(int argc, char* argv[]) {
+auto main(int argc, char* argv[]) -> int {
     try {
-        bool test = false;
+        bool test;
 
         // hyperparameters
         int numFeatures = 6;
-        int hiddenSize = 32;  // dimension of lstm matrices
+        int hiddenSize = 32;     // dimension of lstm matrices
         int sequenceLength = 20; // number of days per sequence
-        int batchSize = 10; // number of sequences per batch
+        int batchSize = 10;      // number of sequences per batch
         double learningRate = 1e-3;
         size_t windowSize = 256; // number of days in each scaler window
         double maxNorm = 0.001;
         double decayFactor = 0.5; // learning rate halves at each plateau
-        double minLR = 1e-6; // minimum learning rate
-        int lrDecayMaxTries = 3; // gives up after 3 decays
-        double delta = 1.0; // huber loss delta value
-        int epochs = 30;
-        double stoppingToleranceLoss = 1e-4;
-        int maxEpochsWithNoImprovement = 3;
+        double minLR = 1e-6;      // minimum learning rate
+        int lrDecayMaxTries = 3;  // gives up after 3 decays
+        double delta = 1.0;       // huber loss delta value
+        int epochs;
+        double stoppingToleranceLoss;
+        int maxEpochsWithNoImprovement;
 
-        for(int i = 1; i < argc; ++i) {
-            std::string arg = argv[i];
-            if(arg == "--epochs" && i+1 < argc) {
-                epochs = std::stoi(argv[++i]);
-            }else if(arg == "--early-stop-eps" && i+1 < argc) {
-                stoppingToleranceLoss = std::stod(argv[++i]);
-            }else if(arg == "--early-stop-patience" && i+1 < argc) {
-                maxEpochsWithNoImprovement = std::stoi(argv[++i]);
-            }else if(arg == "--test"){
-                test = true;
-            }else if(arg == "--help") {
-                std::cout
-                  << "Usage: " << argv[0] << " [options]\n"
-                  << "Options:\n"
-                  << "  --epochs N                     Train up to N epochs (default 10)\n"
-                  << "  --early-stop-eps X             Early-stop tol. (default 1e-3)\n"
-                  << "  --early-stop-patience P        Early-stop patience (default 3)\n"
-                  << "  --test                         Run hyperparameter search\n";
-                return 0;
-            }
+        cxxopts::Options options {"Stock Bot", ""};
+        options.add_options()
+			("e,epochs", "Train up to N epochs", cxxopts::value<int>()->default_value("30"))
+			("early-stop-eps", "Early stop tol", cxxopts::value<double>()->default_value("1e-4"))
+			("early-stop-patience", "Early stop patience", cxxopts::value<int>()->default_value("3"))
+			("t,test", "Run hyperparameter search")
+			("h,help", "Print usage");
+        auto result = options.parse(argc, argv);
+
+        if (result.contains("help")) {
+            fmt::println("{}", options.help());
+            return 0;
         }
+
+        stoppingToleranceLoss = result["early-stop-eps"].as<double>();
+        maxEpochsWithNoImprovement = result["early-stop-patience"].as<int>();
+        epochs = result["epochs"].as<int>();
+        test = result["test"].as<bool>();
+
+        // for (int i = 1; i < argc; ++i) {
+        //     std::string arg = argv[i];
+        //     if (arg == "--epochs" && i + 1 < argc) {
+        //         epochs = std::stoi(argv[++i]);
+        //     } else if (arg == "--early-stop-eps" && i + 1 < argc) {
+        //         stoppingToleranceLoss = std::stod(argv[++i]);
+        //     } else if (arg == "--early-stop-patience" && i + 1 < argc) {
+        //         maxEpochsWithNoImprovement = std::stoi(argv[++i]);
+        //     } else if (arg == "--test") {
+        //         test = true;
+        //     } else if (arg == "--help") {
+        //         std::cout << "Usage: " << argv[0] << " [options]\n"
+        //                   << "Options:\n"
+        //                   << "  --epochs N                     Train up to N
+        //                   "
+        //                      "epochs (default 10)\n"
+        //                   << "  --early-stop-eps X             Early-stop
+        //                   tol. "
+        //                      "(default 1e-3)\n"
+        //                   << "  --early-stop-patience P        Early-stop "
+        //                      "patience (default 3)\n"
+        //                   << "  --test                         Run "
+        //                      "hyperparameter search\n";
+        //         return 0;
+        //     }
+        // }
 
         // load data
         const std::string csvPath = "data/AAAU.csv";
         CSVLoader loader(csvPath);
         const std::vector<PriceData> rawData = loader.getData();
-        std::cout << "parsed " << rawData.size() << " rows from CSV" << std::endl;
+
+        fmt::println("Parsed {} rows from CSV", rawData.size());
 
         // split data into 80/20 train/validation split
-        int maxStartSequence = rawData.size() - sequenceLength;  
-        int splitStart = 0.8*maxStartSequence;
-        
-        std::vector<PriceData> trainingData(rawData.begin(), rawData.begin() + (splitStart + sequenceLength));
-        std::vector<PriceData> validationData(rawData.begin() + splitStart, rawData.end());
+        int maxStartSequence = rawData.size() - sequenceLength;
+        int splitStart = 0.8 * maxStartSequence;
 
-        if(test){
+        std::vector<PriceData> trainingData(
+            rawData.begin(), rawData.begin() + (splitStart + sequenceLength)
+        );
+        std::vector<PriceData> validationData(
+            rawData.begin() + splitStart, rawData.end()
+        );
+
+        if (test) {
             std::vector<HyperParam> params = {
-                {"hiddenSize",     ParamType::INTEGER, {16,   32,   64  }},
-                {"sequenceLength", ParamType::INTEGER, {10,   20,   40  }},
-                {"batchSize",      ParamType::INTEGER, {16,   32,   64  }},
-                {"learningRate",   ParamType::DOUBLE,  {1e-3, 1e-4, 1e-5}},
-                {"windowSize",   ParamType::INTEGER,  {64, 128, 256}},
-                {"delta",          ParamType::DOUBLE,  {0.5,  1.0,  2.0}}
+                {"hiddenSize", ParamType::INTEGER, {16, 32, 64}},
+                {"sequenceLength", ParamType::INTEGER, {10, 20, 40}},
+                {"batchSize", ParamType::INTEGER, {16, 32, 64}},
+                {"learningRate", ParamType::DOUBLE, {1e-3, 1e-4, 1e-5}},
+                {"windowSize", ParamType::INTEGER, {64, 128, 256}},
+                {"delta", ParamType::DOUBLE, {0.5, 1.0, 2.0}}
             };
-    
+
             gridSearch(
                 params,
                 numFeatures,
@@ -93,12 +119,12 @@ int main(int argc, char* argv[]) {
                 stoppingToleranceLoss,
                 maxEpochsWithNoImprovement
             );
-        }else{
+        } else {
             Trainer trainer(
                 numFeatures,
                 hiddenSize,
                 sequenceLength,
-                batchSize, 
+                batchSize,
                 learningRate,
                 delta,
                 windowSize,
@@ -109,10 +135,11 @@ int main(int argc, char* argv[]) {
                 trainingData,
                 validationData
             );
-    
-            trainer.run(epochs, stoppingToleranceLoss, maxEpochsWithNoImprovement);
-        }
 
+            trainer.run(
+                epochs, stoppingToleranceLoss, maxEpochsWithNoImprovement
+            );
+        }
 
     } catch (const std::exception& ex) {
         std::cerr << "Error: " << ex.what() << std::endl;
